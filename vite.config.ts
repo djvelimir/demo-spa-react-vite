87c0b0b43e94c120b1fc15f97324e0b010f6596c
Velimir Đurković <velimir.djurkovic@gmail.com>
2025-02-24 02:05:50 +0100

    feat: upgrade tailwind css from v3 to v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/demo-spa-react-vite/',
-  plugins: [react()],
-})
+  base: "/demo-spa-react-vite/",
+  plugins: [tailwindcss(), react()],
+});
